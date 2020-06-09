40f464fffa9c9c003dee0807c60ac747ddf6a92a
Emma Hall <osboxes@osboxes.com>
2020-06-09 13:58:51 -0400

    basic index page added, waiting on edit/create forms to be connected

---
+++ src/App/shop.js
@@ -0,0 +1,112 @@
+import React from 'react';
+
+import './style.css';
+// import Form from './form.js'
+const Shop = (props) => {
+   
+    const [products, setProducts] = React.useState(null);
+    const [editProduct, setEditProduct] = React.useState({
+        name: '',
+        description: '',
+        image: '',
+        price: 0,
+        quantity: 0
+    });
+    
+    const blank = {
+        name: '',
+        description: '',
+        image: '',
+        price: 0,
+        quantity: 0
+    };
+    //Function to get products from API
+    const getInfo = async () => {
+        const response = await fetch('http://localhost:3001/products', {
+            
+        });
+        const result = await response.json();
+        console.log(result);
+        setProducts(result);
+    };
+    
+    React.useEffect(()=>{
+        getInfo()
+    }, []) 
+
+
+    const handleCreate = async (data) => {
+        const response = await fetch('http://localhost:3001/products', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json' 
+            },
+            body: JSON.stringify(data),
+        });
+        getInfo(); 
+    };
+
+    const handleDelete = async (id) => {
+        const response = await fetch(`http://localhost:3001/products/${id}`, {
+            method: 'DELETE',
+           
+        });
+       
+        getInfo();
+    };
+  
+    const handleSelect = async (product) => {
+        setEditProduct(product);
+    };
+    const handleEdit = async (data) => {
+        const response = await fetch(
+            `http://localhost:3001/products/${data._id}`,
+            {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'  
+                },
+                body: JSON.stringify(data),
+            }
+        );
+        getInfo();
+    };
+ 
+    return  (
+        <>
+            <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">App Name</h1>
+                <p className="lead">Some small description of app goes here</p>
+            </div>
+            </div>
+            <h1>Add Product</h1>
+            {/* <Form initial={blank} handleSubmit={handleCreate} /> */}
+            <h1>Edit Product</h1>
+            {/* <Form initial={editProduct} handleSubmit={handleEdit} /> */}
+            
+                {products
+                    ? products.map((product) => {
+                          return (
+                            <div key={product._id} className="card .d-flex" style={{width: "18rem"}}>
+                            <img src={product.image} className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                              <span className="card-title">{product.name}</span><span className="card-title">${product.price}</span> 
+                              <p className="card-text">{product.description}</p>
+                              <a href="#" className="btn btn-primary">Add to Cart</a>
+                              <button className="btn btn-dark" onClick={() => {
+                               handleDelete(product._id)
+                           }}>X</button>
+                            </div>
+
+                          </div>
+                             
+                          );
+                      })
+                    : 'LOADING...'}
+            
+            
+        </>
+    ) 
+};
+export default Shop;
