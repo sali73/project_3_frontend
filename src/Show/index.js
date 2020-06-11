0a188c596715e59d7d6fb3f74a86f218d9abec4a
sali mohamed <sally.elgendy@hotmail.com>
2020-06-11 07:56:48 -0400

    before start working

--- src/Show/index.js
+++ src/Show/index.js
@@ -8,9 +8,9 @@ function Show(props) {
             {/* <h1>Show Product:</h1> */}
             <div><h2 style={{}}>{product.name}</h2></div>
             <img src={product.image} alt="..." style={{width:'350px'}}/>
-            <h4>price${product.price}</h4>
+            <h4>${product.price}</h4>
             <h5>Q:{product.quantity}</h5>
-            <button>ADD TO CART</button>
+            <button type="button" class="btn btn-secondary btn-sm">ADD TO CART</button>
 
             <h5>Product Details:<h6>{product.description}</h6></h5>
             
