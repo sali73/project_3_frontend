20b5bbee60edd24f1ac801341b4963303debadeb
sali mohamed <sally.elgendy@hotmail.com>
2020-06-12 18:07:49 -0400

    add more style to our show componont

--- src/Show/index.js
+++ src/Show/index.js
@@ -1,4 +1,7 @@
 import React from 'react'
+// import '@fortawesome/fontawesome-free/css/all.min.css'; 
+// import'bootstrap-css-only/css/bootstrap.min.css'; 
+// import'mdbreact/dist/css/mdb.css';
 
 function Show(props) {
     const product = props.location.aboutProps;
@@ -10,13 +13,10 @@ function Show(props) {
            </head>
 
             <div className='jumbotron-fluid ' style={{textAlign:'center',fontFamily:"Abril Fatface"}}>
-
-                <div style={{display:'flex' , paddingTop:"5vh" , justifyContent:'center' }}>
-                    
-                    <div>
-                        <img src={product.image} style={{width:'300px'}}/>
+                <div style={{display:'flex' , paddingTop:"5vh" , justifyContent:'center' }}>   
+                    <div className="view overlay zoom">
+                        <img src={product.image} className="img-fluid " alt="smaple image" style={{height:'300px'}}/>      
                     </div>
-
                     <div style={{ paddingTop:'30px' , textAlign:'left' , paddingLeft:'20px'}}>
                         <h3 style={{lineHeight:'1.6'}}>Product Name: {product.name}</h3>
                         <h4 style={{lineHeight:'1.6'}}>Price: ${product.price}</h4>
@@ -24,12 +24,8 @@ function Show(props) {
                         <button type="button" class="btn btn-secondary btn-sm">ADD TO CART</button>
                     </div>
                 </div>
-              
-
                 <div style={{borderStyle: 'groove' , margin:'20px 180px', fontSize:"3vh"}}><h5>Product Details:</h5><p style={{fontFamily:'aril'}}>{product.description}</p></div>
-            
             </div>  
-            
         </>
     )
 }
