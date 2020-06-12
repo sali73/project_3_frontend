c14b814cf030b94763312c4fa818678027aabb99
sali mohamed <sally.elgendy@hotmail.com>
2020-06-12 15:49:07 -0400

    add more style to our footer

--- src/Show/index.js
+++ src/Show/index.js
@@ -5,15 +5,18 @@ function Show(props) {
     console.log(props.location)
     return (
         <>
-            <div className='jumbotron-fluid ' style={{textAlign:'center'}}>
+           <head>
+           <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet"></link>
+           </head>
+            <div className='jumbotron-fluid ' style={{textAlign:'center',fontFamily:"Abril Fatface"}}>
             {/* <h1>Show Product:</h1> */}
-            <div><h2 style={{}}>{product.name}</h2></div>
-            <img src={product.image} alt="..." style={{width:'350px'}}/>
-            <h4>price${product.price}</h4>
-            <h5>Q:{product.quantity}</h5>
-            <button>ADD TO CART</button>
+            <div style={{fontSize:"10vh"}}><h2>{product.name}</h2></div>
+            <img src={product.image} alt="..." style={{width:'300px'}} />
+            <h4 style={{fontSize:"4vh"}}>${product.price}</h4>
+            <h5 style={{fontSize:"3.5vh"}}>Quantity:{product.quantity}</h5>
+            <button type="button" class="btn btn-primary">ADD TO CART</button>
 
-            <h5>Product Details:<p>{product.description}</p></h5>
+            <div style={{borderStyle: 'groove' , margin:'20px 180px', fontSize:"3vh"}}><h5>Product Details:</h5><p style={{fontFamily:'aril'}}>{product.description}</p></div>
             
             </div>  
             
