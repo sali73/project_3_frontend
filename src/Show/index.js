18cfc878d4ea6bb177e1d12c7e63c630da9bac4b
christian <ceunger91@gmail.com>
2020-06-13 17:22:52 -0400

    merging

--- src/Show/index.js
+++ src/Show/index.js
@@ -16,13 +16,13 @@ function Show(props) {
             <div className='jumbotron-fluid ' style={{textAlign:'center',fontFamily:"Abril Fatface"}}>
                 <div style={{display:'flex' , paddingTop:"5vh" , justifyContent:'center' }}>   
                     <div className="view overlay zoom">
-                        <img src={product.image} className="img-fluid " alt="smaple image" style={{height:'300px'}}/>      
+                        <img src={product.image} className="img-fluid" alt="sample" style={{height:'300px'}}/>      
                     </div>
                     <div style={{ paddingTop:'30px' , textAlign:'left' , paddingLeft:'20px'}}>
                         <h3 style={{lineHeight:'1.6'}}>Product Name: {product.name}</h3>
                         <h4 style={{lineHeight:'1.6'}}>Price: ${product.price}</h4>
                         <h5 style={{lineHeight:'1.6'}}>Quantity: {product.quantity}</h5><br/>
-                        <button type="button" class="btn btn-secondary btn-sm">ADD TO CART</button>
+                        <AddToCart />
                     </div>
                 </div>
                 <div style={{borderStyle: 'groove' , margin:'20px 180px', fontSize:"3vh"}}><h5>Product Details:</h5><p style={{fontFamily:'aril'}}>{product.description}</p></div>
